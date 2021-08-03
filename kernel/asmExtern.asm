b591050b840e7805c2329c1aa6d3dd12757ce0a2
verticalfile30 <stopmotion45b@gmail.com>
2021-08-03 12:31:12 -0400

    Revert "ddd"
    This reverts commit 0c7ad9631bc6eebb37cfcf16f02ced3beb7c76e8.
    
    sisdfneccessar changes#

--- kernel/asmExtern.asm
+++ kernel/asmExtern.asm
@@ -6,28 +6,13 @@ global _start
 section .text
 _start:
 asm_add:
-
-    xchg bx, bx
-
-    pusha
-   
-    xchg bx, bx 
- 
     mov [base_pointer_store], ebp ;create new stack
     mov ebp, esp
     mov eax, [esp + 8] ;fetch paramenters
     mov ebx, [esp + 12]
     add eax, ebx
     mov ebp, [base_pointer_store] ;restore pre fn call stack
-    
-    mov [fnReturn], eax
-    xchg bx, bx 
-    popa
-    xchg bx, bx
-    mov eax, [fnReturn]
-    xchg bx, bx
     ret
 
 
 base_pointer_store: dw 0
-fnReturn: dw 0
